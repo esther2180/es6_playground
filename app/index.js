dc550a3e3c1509eaf49a0ca8f271698e4e66cd61
Esther <esther2180@gmail.com>
2018-09-12 14:49:55 -0700

    Super Class and Extends

--- app/index.js
+++ app/index.js
@@ -1,14 +1,16 @@
 // import $ from 'jquery';
-
 // $('#root').append('<h1>This is from jQuery!</h1>');
-
-import addToDom from './add_to_dom';
+// import addToDom from './add_to_dom';
 // import { subtract } from './math';
-import string {subtract} from './math';
-
+// import string {subtract} from './math';
 // import * as math from './math';
-
-console.log('Math:', math);
-
+// console.log('Math:', math);
 // addToDom('h1', `52 - 10 = ${subtract(52, 10)}`);
 
+import SuperPerson from './person';
+
+const bob = new SuperPerson('Arden', 'Love cooking!');
+
+bob.greet();
+
+bob.saySuperPower();
